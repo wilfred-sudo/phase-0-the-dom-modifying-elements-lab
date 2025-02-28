a326f659e1fb86e9cb184c14576784f5bbfe767a
wilfredkiprop <kipropwilfred90@gmail.com>
2025-02-28 16:09:39 +0300

    modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+if (main) {
+    main.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id ="victory"
+newHeader.textContent= "wilfred is the champion"
+document.body.appendChild(newHeader);
